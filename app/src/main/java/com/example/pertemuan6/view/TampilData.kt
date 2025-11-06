edcfca036fa7d262743b3efb3fdff6fcc9cbee62
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-06 17:40:24 +0700

    Menggunakan Scaffold dengan TopAppBar berwarna teal_700 dan judul dari string resources

--- app/src/main/java/com/example/pertemuan6/view/TampilData.kt
+++ app/src/main/java/com/example/pertemuan6/view/TampilData.kt
@@ -30,5 +30,23 @@ import com.example.pertemuan6.R
 fun TampilData(
     onBackBtnClick:()->Unit
 ){
-
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = ), "Yogyakarta"),
+    )
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id=R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+            )
+        }
+    ){}
 }
