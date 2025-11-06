577dd6a6e856a4c576dbff80f808871806c04996
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-11-06 17:54:27 +0700

    tambahkan fungsi cancelAndBackToFormulirku untuk kembali ke halaman Formulirku

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -44,3 +44,9 @@ fun DataApp(
         }
     }
 }
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
+}
